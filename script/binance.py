a724cd4f294786dde6e64e42de2768d69970cf9c
Marine Bauerlé <145704547+MarineBauerle@users.noreply.github.com>
2023-11-03 12:09:22 +0000

    Update binance.py

--- script/binance.py
+++ script/binance.py
@@ -20,3 +20,21 @@ print("Potential Outliers:")
 print(outliers)
 
 data.to_csv('cleaned_data.csv', index=False)
+
+
+### Binance Competitor Analysis Script
+
+#### Description:
+This Python script provides a comprehensive analysis of the competitors' relevance against organic traffic. The script primarily focuses on visualizing the data and identifying potential outliers.
+
+#### Key Features:
+1. **Data Loading and Overview**: Loads a dataset that contains information about competitors' relevance and organic traffic and provides a quick overview of the data.
+2. **Data Visualization**: Creates a scatter plot to showcase the relationship between competitor relevance and organic traffic.
+3. **Outlier Detection**: Uses Z-score calculations to detect potential outliers in the organic traffic data.
+4. **Data Export**: Saves the processed dataset, including calculated z-scores, into a new CSV file for further analysis.
+
+
+#### Potential Enhancements:
+- Incorporation of additional outlier detection methods.
+- Enhanced visualizations, such as histograms or box plots.
+- Advanced statistical analysis to derive more insights from the data.
